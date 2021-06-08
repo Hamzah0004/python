37c20f848f6eab542acc41120643c6bae71206df
Memeber <85556698+Hamzah0004@users.noreply.github.com>
2021-06-08 16:58:46 +0530

    code.py

---
+++ Code.py
@@ -0,0 +1,7 @@
+radius = int(input("radius: "))
+area = 3.14 * radius * radius
+print("The area of the circle is " )
+print(area)
+file_name = input("Input the filename: ")
+print("The extension of the file is:")
+print("'Python'")
